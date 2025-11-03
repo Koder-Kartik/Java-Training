368bc2b385e1f4833f22131fa21ae4461c13eaa4
Kartik Jain <koderkartik@gmail.com>
2025-11-03 18:57:59 +0530

    temperature code

---
+++ Basics/src/second_code_temperature.java
@@ -0,0 +1,8 @@
+public class second_code_temperature
+{
+    static void main(String[] args) {
+        int celcius=30;
+        float fh=((9/5)*celcius)+32;
+        System.out.println("The ans is "+fh);
+    }
+}
