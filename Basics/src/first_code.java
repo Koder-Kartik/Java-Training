618c5e72bb5ee53303f99a43fe9bb02e0739fb91
Kartik Jain <koderkartik@gmail.com>
2025-11-03 18:52:06 +0530

    Basic codes

---
+++ Basics/src/first_code.java
@@ -0,0 +1,19 @@
+public class first_code {
+
+    static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        int d=30;
+        int c=a+b+d;
+        System.out.println("The sum of a and b  and d is "+c);
+    }
+
+}
+
+
+
+
+
+
+
