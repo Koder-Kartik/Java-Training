f8567d93f5c1acc9f88961fbb99e6afe16929bf9
Kartik Jain <koderkartik@gmail.com>
2025-11-04 20:13:54 +0530

    java day-2

--- Basics/src/second_code_temperature.java
+++ Basics/src/second_code_temperature.java
@@ -1,7 +1,7 @@
 public class second_code_temperature
 {
     static void main(String[] args) {
-        int celcius=30;
+       int celcius=30;
         float fh=((9/5)*celcius)+32;
         System.out.println("The ans is "+fh);
     }
---
+++ Basics/src/third_simple_interest.java
@@ -0,0 +1,11 @@
+public class third_simple_interest
+{
+    static void main(String[] args) {
+        int P=30000;
+        int R=5;
+        int T=3;
+        float SI= (P*R*T)/100;
+        System.out.println("The simple interest is is "+SI);
+    }
+
+}
