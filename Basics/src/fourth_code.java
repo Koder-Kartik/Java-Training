b7735989044b20e77960544e38d7d69e84634e64
Kartik Jain <koderkartik@gmail.com>
2025-11-06 11:13:58 +0530

    fourth

---
+++ Basics/src/fourth_code.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class fourth_code
+{
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the age and Height");
+        int age=sc.nextInt();
+        float height=sc.nextFloat();
+        System.out.println("Your age is "+age+" And " + "your height is "+height);
+
+    }
+
+}
