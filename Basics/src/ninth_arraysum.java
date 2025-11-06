62c9dd484d2dce64a7762d2b2b432e92c94e8455
Kartik Jain <koderkartik@gmail.com>
2025-11-06 11:36:29 +0530

    ninth

---
+++ Basics/src/ninth_arraysum.java
@@ -0,0 +1,11 @@
+public class ninth_arraysum
+{
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [] arr=new int[5];
+        for(int i=0; i<arr.length;i++)
+        {
+            arr[i]=
+        }
+    }
+}
