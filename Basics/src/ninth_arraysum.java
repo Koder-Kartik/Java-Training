2a57afdf2b60b541ce14ad90a698c11255378aa4
Kartik Jain <koderkartik@gmail.com>
2025-11-07 10:45:37 +0530

    ninth

--- Basics/src/ninth_arraysum.java
+++ Basics/src/ninth_arraysum.java
@@ -1,11 +1,18 @@
+import java.util.*;
 public class ninth_arraysum
 {
     static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int [] arr=new int[5];
-        for(int i=0; i<arr.length;i++)
-        {
-            arr[i]=
-        }
+        int [] arr= {3,4,5,6,7};
+
+
+            int sum = 0;
+            for(int i = 0; i < arr.length; i++){
+                sum = sum + arr[i];
+            }
+            System.out.println("Sum = " + sum);
+
+
+
     }
 }
