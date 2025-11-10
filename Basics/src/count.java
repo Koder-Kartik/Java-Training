87e8393474d9924aade41d5e7d4f8483986acbca
Kartik Jain <koderkartik@gmail.com>
2025-11-10 19:41:26 +0530

    eleventh

---
+++ Basics/src/count.java
@@ -0,0 +1,14 @@
+import java.util.*;
+    public class count {
+    {
+        public static void main(String args[])
+        {
+            scanner in = new scanner(System.in);
+            String s1;
+            s1 = in.nextLine();
+            system.out.println("Enter the String");
+            s1 = str.lenght();
+            system.out.println("the lenght of string is " +s1);
+        }
+    }
+}
